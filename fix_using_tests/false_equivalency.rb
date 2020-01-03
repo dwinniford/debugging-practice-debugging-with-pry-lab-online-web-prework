0b22d4c25836dc5664d9dd0d351b56121ac6bb07
darling-flag-8189 <dwinniford@gmail.com>
2020-01-03 02:33:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-
+requre 'pry'
 
 def get_user_input
   gets.chomp
@@ -13,6 +13,7 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
+     binding.pry 
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
   end
